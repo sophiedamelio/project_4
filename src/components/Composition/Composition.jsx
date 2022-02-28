9cbbff46e9b67ce9fff8233824f3dc9549269313
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-02-27 16:29:14 -0800

    Made auto-scroll button 'sticky' so it stays in view when the page scrolls

--- src/components/Composition/Composition.jsx
+++ src/components/Composition/Composition.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import { Grid, Image, Modal, Button } from 'semantic-ui-react';
+import React, { useState, createRef } from 'react';
+import { Modal, Rail, Ref, Segment, Sticky, Grid } from 'semantic-ui-react';
 import "./Composition.css";
+import _ from 'lodash'
 
 import UpdateCompositionForm from '../UpdateCompositionForm/UpdateCompositionForm';
+
 
 export default function Composition(props) {
 
@@ -10,40 +12,51 @@ export default function Composition(props) {
 
 	console.log(props, "<--- props in composition component")
 
+
+	const contextRef = createRef()
+
 	if (props.selectedComposition) {
 
 		return (
 			<div id="composition">
-				<div id="comp-header">
-					<Grid>
-						<Grid.Row>
-							<Grid.Column width="10">
-								<span><h3 id="title">{props.selectedComposition.title}</h3></span>
-							</Grid.Column>
-							<Grid.Column width="6">
-							</Grid.Column>
-						</Grid.Row>
-					</Grid>
-				</div>
-				<button id="scroll-button" onClick={props.pageScroll}>auto-scroll</button>
-				<p>{props.selectedComposition.text}</p>
-				<p id="notes"><span id="notes-label">notes: <br></br></span>{props.selectedComposition.notes}</p>
-				<Modal
-					style={{ borderRadius: "20px" }}
-					onClose={() => setOpen(false)}
-					onOpen={() => setOpen(true)}
-					open={open}
-					trigger={<button id="edit-comp-btn">edit composition</button>}>
-					<Modal.Header style={{ backgroundColor: "#1f2024", color: "white", fontFamily: "'Major Mono Display', monospace" }}>Edit - {props.selectedComposition.title}</Modal.Header>
-					<Modal.Content style={{ backgroundColor: "#3a3b42" }}>
-						<UpdateCompositionForm {...props}></UpdateCompositionForm>
-						<Modal.Actions>
-							<button content="Close Update Mode" id="close-modal-btn" onClick={() => setOpen(false)} >Close Update Mode</button>
-						</Modal.Actions>
-					</Modal.Content>
-				</Modal>
-				{/* I use .bind so that I can pass an argument into the handleDeleteComposition function inline */}
-				<button id="delete-comp-btn" onClick={props.handleDeleteComposition.bind(null, props.selectedComposition._id)}>Delete Composition</button>
+				<Grid columns={2}>
+					<Grid.Column>
+						<Ref innerRef={contextRef}>
+							<Segment style={{ backgroundColor: '#3a3b42', border: 'none' }}>
+								{_.times(1, (i) => (
+									<>
+										<div id="comp-header">
+											<span><h3 id="title">{props.selectedComposition.title}</h3></span>
+										</div>
+										<p>{props.selectedComposition.text}</p>
+										<p id="notes"><span id="notes-label">notes: <br></br></span>{props.selectedComposition.notes}</p>
+										<Modal
+											style={{ borderRadius: "20px" }}
+											onClose={() => setOpen(false)}
+											onOpen={() => setOpen(true)}
+											open={open}
+											trigger={<button id="edit-comp-btn">edit composition</button>}>
+											<Modal.Header style={{ backgroundColor: "#1f2024", color: "white", fontFamily: "'Major Mono Display', monospace" }}>Edit - {props.selectedComposition.title}</Modal.Header>
+											<Modal.Content style={{ backgroundColor: "#3a3b42" }}>
+												<UpdateCompositionForm {...props}></UpdateCompositionForm>
+												<Modal.Actions>
+													<button content="Close Update Mode" id="close-modal-btn" onClick={() => setOpen(false)} >Close Update Mode</button>
+												</Modal.Actions>
+											</Modal.Content>
+										</Modal>
+										{/* I use .bind so that I can pass an argument into the handleDeleteComposition function inline */}
+										<button id="delete-comp-btn" onClick={props.handleDeleteComposition.bind(null, props.selectedComposition._id)}>Delete Composition</button>
+									</>
+								))}
+								<Rail position="right">
+									<Sticky context={contextRef}>
+										<button id="scroll-button" onClick={props.pageScroll}>auto-scroll</button>
+									</Sticky>
+								</Rail>
+							</Segment>
+						</Ref>
+					</Grid.Column>
+				</Grid>
 			</div >
 		)
 	}

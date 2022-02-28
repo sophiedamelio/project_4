7bcbb75a67ee452c32cf1d46e1f95fa671349f15
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-02-28 12:23:22 -0800

    Work on setting initial selected composition, still not functioning

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -17,7 +17,18 @@ export default function HomePage({ user, handleLogout, }) {
 	const firstComposition = compositions[0];
 	console.log(firstComposition, "<--- first comp")
 
-	const [selectedComposition, setSelectedComposition] = useState('')
+	const [selectedComposition, setSelectedComposition] = useState({
+		//_id: firstComposition._id,
+		//user: firstComposition.user,
+		//title: firstComposition.title,
+		//capo: firstComposition.capo,
+		//text: firstComposition.text,
+		//notes: firstComposition.notes
+		//compositions[0]
+	})
+
+	console.log(selectedComposition, "<--- initial selected comp")
+
 
 	function startPageScroll() {
 		window.scrollBy(0, 1);

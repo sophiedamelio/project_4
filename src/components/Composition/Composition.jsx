d28a277b942dbf2996d1b6759f0864e7fc87296b
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-03-01 13:49:34 -0800

    Styling

--- src/components/Composition/Composition.jsx
+++ src/components/Composition/Composition.jsx
@@ -29,41 +29,45 @@ export default function Composition(props) {
 		return (
 			<div id="composition">
 				<Grid columns={2}>
-					<Grid.Column>
+					<Grid.Row>
 						<Ref innerRef={contextRef}>
 							<Segment id="segment-composition" style={{ backgroundColor: '#3a3b42', border: 'none' }}>
-								<div key={props.selectedComposition._id}>
-									<div id="comp-header">
-										<span><h3 id="title">{props.selectedComposition.title}</h3></span>
+								<Grid.Column>
+									<div key={props.selectedComposition._id}>
+										<div id="comp-header">
+											<span><h3 id="title">{props.selectedComposition.title}</h3></span>
+										</div>
+										{props.selectedComposition.capo ? <p>Capo: {props.selectedComposition.capo}</p> : null}
+										<p>{props.selectedComposition.text}</p>
+										{props.selectedComposition.notes ?
+											<p id="notes"><span id="notes-label">notes: <br></br></span>{props.selectedComposition.notes}</p>
+											: null}
+										<Modal
+											style={{ borderRadius: "20px" }}
+											onClose={() => setOpen(false)}
+											onOpen={() => setOpen(true)}
+											open={open}
+											trigger={<button id="edit-comp-btn">edit composition</button>}>
+											<Modal.Header style={{ backgroundColor: "#1f2024", color: "white", fontFamily: "'Major Mono Display', monospace" }}>Edit - {props.selectedComposition.title}</Modal.Header>
+											<Modal.Content style={{ backgroundColor: "#3a3b42" }}>
+												<UpdateComposition setOpen={setOpen} {...props}></UpdateComposition>
+											</Modal.Content>
+										</Modal>
+										{/* I use .bind so that I can pass an argument into the handleDeleteComposition function inline */}
+										<button id="delete-comp-btn" onClick={props.handleDeleteComposition.bind(null, props.selectedComposition._id)}>Delete Composition</button>
 									</div>
-									{props.selectedComposition.capo ? <p>Capo: {props.selectedComposition.capo}</p> : null}
-									<p>{props.selectedComposition.text}</p>
-									{props.selectedComposition.notes ?
-										<p id="notes"><span id="notes-label">notes: <br></br></span>{props.selectedComposition.notes}</p>
-										: null}
-									<Modal
-										style={{ borderRadius: "20px" }}
-										onClose={() => setOpen(false)}
-										onOpen={() => setOpen(true)}
-										open={open}
-										trigger={<button id="edit-comp-btn">edit composition</button>}>
-										<Modal.Header style={{ backgroundColor: "#1f2024", color: "white", fontFamily: "'Major Mono Display', monospace" }}>Edit - {props.selectedComposition.title}</Modal.Header>
-										<Modal.Content style={{ backgroundColor: "#3a3b42" }}>
-											<UpdateComposition setOpen={setOpen} {...props}></UpdateComposition>
-										</Modal.Content>
-									</Modal>
-									{/* I use .bind so that I can pass an argument into the handleDeleteComposition function inline */}
-									<button id="delete-comp-btn" onClick={props.handleDeleteComposition.bind(null, props.selectedComposition._id)}>Delete Composition</button>
-								</div>
-								<Rail position="right">
-									<Sticky context={contextRef}>
-										<button id="start-scroll-button" onClick={props.startPageScroll}>start auto-scroll</button>
-										<button id="stop-scroll-button" onClick={props.stopPageScroll}>stop auto-scroll</button>
-									</Sticky>
-								</Rail>
+								</Grid.Column>
+								<Grid.Column>
+									<Rail position="right">
+										<Sticky context={contextRef}>
+											<button id="start-scroll-button" onClick={props.startPageScroll}>start auto-scroll</button>
+											<button id="stop-scroll-button" onClick={props.stopPageScroll}>stop auto-scroll</button>
+										</Sticky>
+									</Rail>
+								</Grid.Column>
 							</Segment>
 						</Ref>
-					</Grid.Column>
+					</Grid.Row>
 				</Grid>
 			</div >
 		)

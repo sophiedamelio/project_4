5eedc173be723baee9c8a4ef1de72429b8030781
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-05-26 14:25:04 -0700

    clean up models

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ userSchema.set('toJSON', {
     return ret;
   }
 });
-/// in controller
+
 
 // this is if you populate the user
 userSchema.set('toObject', {
@@ -34,8 +34,7 @@ userSchema.set('toObject', {
 userSchema.pre('save', function(next) {
   // 'this' will be set to the current document
   const user = this;
-  // check to see if the user has been modified, if not proceed 
-  // in the middleware chain
+  // check to see if the user has been modified, if not proceed in the middleware chain
   if (!user.isModified('password')) return next();
   // password has been changed - salt and hash it
   bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
@@ -47,7 +46,6 @@ userSchema.pre('save', function(next) {
 });
 
 userSchema.methods.comparePassword = function(tryPassword, cb) {
-    console.log(cb, ' this is cb')
   // 'this' represents the document that you called comparePassword on
   bcrypt.compare(tryPassword, this.password, function(err, isMatch) {
     if (err) return cb(err);

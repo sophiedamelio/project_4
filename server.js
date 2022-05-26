cc4ea34d0e77d9e37ae925174dd3022188e4db57
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-05-26 14:06:17 -0700

    Clean up comments in server.js

--- server.js
+++ server.js
@@ -6,21 +6,16 @@ const favicon = require('serve-favicon');
 
 require('./config/database');
 
-// Require controllers here
-
 const app = express();
 
-// add in when the app is ready to be deployed
-// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
-
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build'))); // this allows express to find the build folder
 // Configure the auth middleware
-// This decodes the jwt token, and assigns
-// the user information to req.user
+// ./config/auth.js decodes the jwt token, and assigns the user information to req.user
 app.use(require('./config/auth')); 
+
 // api routes must be before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/compositions', require('./routes/api/compositions'));

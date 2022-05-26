6969e1cb27b4b35a8cca9ad60c50f3742956cf55
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-05-26 14:22:36 -0700

    clean up routes/api

--- routes/api/compositions.js
+++ routes/api/compositions.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 const compositionsCtrl = require('../../controllers/compositions');
 
-/*---------- Public Routes ----------*/
+/*---------- Protected Routes (all are private) ----------*/
 router.get('/', compositionsCtrl.index);
 router.post('/', isAuthenticated, compositionsCtrl.create);
 router.put('/:compId', isAuthenticated, compositionsCtrl.update);
 router.delete('/:compId', isAuthenticated, compositionsCtrl.deleteComposition);
 
-
-/*---------- Protected Routes ----------*/
 function isAuthenticated(req, res, next){
 	if(req.user){
 		next()
@@ -17,7 +15,6 @@ function isAuthenticated(req, res, next){
 		res.status(401).json({data: 'Not Authorized!'})
 	}
 }
-
 
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/users');
-//const multer = require('multer');
-//const upload = multer();
 
 /*---------- Public Routes ----------*/
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
-
-/*---------- Protected Routes ----------*/
-
-
 
 
 module.exports = router;
